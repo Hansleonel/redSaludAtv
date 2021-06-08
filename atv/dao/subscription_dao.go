f9bf9dbefbf59d5120169902bb885b790573657f
hans <hansleonel@gmail.com>
2021-06-08 17:22:32 -0500

    fix method update stepTwo

--- atv/dao/subscription_dao.go
+++ atv/dao/subscription_dao.go
@@ -23,7 +23,7 @@ func (subscriptionDao SubscriptionDao) Create(subscription *entites.Subscription
 }
 
 func (subscriptionDao SubscriptionDao) CreateStepOne(subscriptionStepOne *entites.SubscriptionStepOne) error {
-	result, err := subscriptionDao.Db.Exec("INSERT INTO asegurado_suscripcion(idtipodocumento, nro_documento, telefono, tipo, tc_datos, idplan, frecuencia_pago, tipo_afiliacion) values(?,?,STR_TO_DATE(? ,'%d-%m-%Y'),?,?,?,?,?,?)", subscriptionStepOne.TypeDoc, subscriptionStepOne.NumDoc, subscriptionStepOne.CelNumber, subscriptionStepOne.Type, subscriptionStepOne.TcDatos, subscriptionStepOne.IdPlan, subscriptionStepOne.FrecuenciaPago, subscriptionStepOne.TipoAfiliacion)
+	result, err := subscriptionDao.Db.Exec("INSERT INTO asegurado_suscripcion(idtipodocumento, nro_documento, telefono, tipo, tc_datos, idplan, frecuencia_pago, tipo_afiliacion) values(?,?,?,?,?,?,?,?)", subscriptionStepOne.TypeDoc, subscriptionStepOne.NumDoc, subscriptionStepOne.CelNumber, subscriptionStepOne.Type, subscriptionStepOne.TcDatos, subscriptionStepOne.IdPlan, subscriptionStepOne.FrecuenciaPago, subscriptionStepOne.TipoAfiliacion)
 	subscriptionDao.Db.Close()
 	if err != nil {
 		return err
@@ -34,7 +34,7 @@ func (subscriptionDao SubscriptionDao) CreateStepOne(subscriptionStepOne *entite
 }
 
 func (subscriptionDao SubscriptionDao) Update(subscriptionStepTwo entites.SubscriptionStepTwo) (int64, error) {
-	result, err := subscriptionDao.Db.Exec("UPDATE asegurado_suscripcion SET fecha_nacimiento = ?, nombre1 = ?, nombre2 = ?, apellido_parteno = ?, apellido_materno = ?, correo = ?, idcontratante = ? WHERE idasegurado_suscripcion = ?", subscriptionStepTwo.FechaNacimiento, subscriptionStepTwo.Nom1, subscriptionStepTwo.Nom2, subscriptionStepTwo.Ape1, subscriptionStepTwo.Ape2, subscriptionStepTwo.Mail, subscriptionStepTwo.Id, subscriptionStepTwo.IdContratante)
+	result, err := subscriptionDao.Db.Exec("UPDATE asegurado_suscripcion SET fecha_nacimiento = ?, nombre1 = ?, nombre2 = ?, apellido_parteno = ?, apellido_materno = ?, correo = ?, idcontratante = ? WHERE idasegurado_suscripcion = ?", subscriptionStepTwo.FechaNacimiento, subscriptionStepTwo.Nom1, subscriptionStepTwo.Nom2, subscriptionStepTwo.Ape1, subscriptionStepTwo.Ape2, subscriptionStepTwo.Mail, subscriptionStepTwo.IdContratante, subscriptionStepTwo.Id)
 	subscriptionDao.Db.Close()
 	if err != nil {
 		return 0, err
